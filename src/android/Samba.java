3d09e1ee7f2869ea2ed85ca1fc19b635e65141be
al3xtc <alexander.unold@gmail.com>
2017-02-03 17:49:08 +0100

    initial commit

---
+++ src/android/Samba.java
@@ -0,0 +1,16 @@
+package org.apache.cordova.samba;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.ConfigXmlParser;
+import org.apache.cordova.LOG;
+import org.apache.cordova.Whitelist;
+import org.xmlpull.v1.XmlPullParser;
+
+import android.content.Context;
+
+public class Samba extends CordovaPlugin {
+    @Override
+    public boolean execute() {
+        return false;
+    }
+}
